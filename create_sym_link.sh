23323a9ce50a01818a9394185e63be35ef5db26c
SATO Yozo <yoozoosato@gmail.com>
2021-11-01 20:46:32 +0900

    リンク作成先を $ZDOTDIR に直した

--- create_sym_link.sh
+++ create_sym_link.sh
@@ -1,6 +1,4 @@
 ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
-#ln -s $HOME/dotfiles/.zlogin $HOME/.zlogin
-ln -s $HOME/dotfiles/.zshenv $HOME/.zshenv
-ln -s $HOME/dotfiles/.zsh/.zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/.zsh/.zaliases $HOME/.zaliases
-ln -s $HOME/dotfiles/.gemrc $HOME/.gemrc
+ln -s $HOME/dotfiles/.zsh/.zshrc $HOME/.zsh/.zshrc
+ln -s $HOME/dotfiles/.zsh/.zaliases $HOME/.zsh/.zaliases
+
